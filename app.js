2a60f23744c7b15738edf7dbe4cf5a54c1d1a435
lmwilson16s <99685187+lmwilson16s@users.noreply.github.com>
2022-02-28 16:20:19 -0600

    finished class 5 exercise

---
+++ app.js
@@ -0,0 +1,10 @@
+const list = [];
+
+const fName = document.querySelector(".fName");
+const lName = document.querySelector(".lName");
+
+function buildList(e) {
+    e.preventDefault();
+    const data = new formData(e.target);
+    list.push();
+}
